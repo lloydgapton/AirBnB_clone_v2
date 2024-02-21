5b240de9581844d7499d5af0088c0cc5cb5201a2
lloydgapton <lloydgapton123@gmail.com>
2024-02-21 14:44:49 -0800

    update

--- models/city.py
+++ models/city.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
-'''
-    Define the class City.
-'''
+""" City Module for HBNB project """
 from models.base_model import BaseModel, Base
-import sqlalchemy
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
 class City(BaseModel, Base):
-    '''
-        Define the class City that inherits from BaseModel.
-    '''
-    __tablename__ = 'cities'
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship('Place', cascade='all, delete', backref='city')
-    else:
-        state_id = ""
-        name = ""
+    """ The city class, contains state ID and name """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", backref="cities", cascade="all, delete")
