540dbda736efce1c2ad16ea42ba061b2de61b8f6
lloydgapton <lloydgapton123@gmail.com>
2024-02-21 07:39:16 -0800

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
+'''
+    Implementation of the Amenity class
+'''
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
+from os import getenv
 from sqlalchemy import Column, String
-from models.place import place_amenity
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """This is the class for Amenity
-    Attributes:
-        name: input name
-    """
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+    '''
+        Implementation for the Amenities.
+    '''
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
