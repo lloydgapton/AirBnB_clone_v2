84ba7f7f608ee4bf0f701bd92464e0053cb9fa78
lloydgapton <lloydgapton123@gmail.com>
2024-03-07 08:27:10 -0800

    updated advanced task

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,20 +1,88 @@
-#update software package
-exec { 'install everything':
-  command  => "
-  sudo apt-get -y update;
-  sudo apt-get -y install nginx;
-  sudo ufw allow 'Nginx HTTP';
-  sudo mkdir -p /data/web_static/releases/test /data/web_static/shared/;
-  echo "<h1>Welcome to the world of computing</h1>" > /data/web_static/releases/test/index.html;
-  if [ -d "/data/web_static/current" ];
-  then
-    sudo rm -rf /data/web_static/current;
-  fi;
-  sudo ln -sf /data/web_static/releases/test/ /data/web_static/current;
-  sudo chown -hR ubuntu:ubuntu /data/;
-  sudo sed -i '11i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default;
-  echo 'Successfully added';
-  sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default';
-  sudo service nginx restart",
-  provider => shell,
+# Configures a web server for deployment of web_static.
+
+# Nginx configuration file
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlw\u4?;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
+} ->
+
+file { '/data':
+  ensure  => 'directory'
+} ->
+
+file { '/data/web_static':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "Holberton School Puppet\n"
+} ->
+
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+
+file { '/var/www':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
+} ->
+
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
