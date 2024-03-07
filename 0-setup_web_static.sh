fab1c1faa4775fe65eee80b59465ff94bf0e707f
lloydgapton <lloydgapton123@gmail.com>
2024-03-07 08:14:04 -0800

    updated file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
-# sets up your web servers for the deployment of web_static
+# Prepare your web servers
+apt -y update
+apt install -y nginx
 
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared/
-echo "<h1>Welcome to the world of computing</h1>" > /data/web_static/releases/test/index.html
-if [ -d "/data/web_static/current" ];
-then
-    sudo rm -rf /data/web_static/current;
-fi;
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '11i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
-echo "Successfully added"
-sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
-sudo service nginx restart
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+
+echo "Holberton School" > /data/web_static/releases/test/index.html
+
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+chown -R ubuntu:ubuntu /data/
+
+sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
+
+service nginx restart
