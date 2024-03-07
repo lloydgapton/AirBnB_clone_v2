347602ddc5e01ed2e34b69cbab4a3c4213c91c65
lloydgapton <lloydgapton123@gmail.com>
2024-03-06 22:51:36 -0800

    update

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,56 +1,54 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug 13 14:21:54 2020
-@author: Robinson Montes
-"""
-from fabric.api import local, put, run, env
+"""Full deployment"""
+from fabric.api import run, local, env, put
 from datetime import datetime
+import os
 
+env.hosts = ['18.210.', '35.168.2.166']
 env.user = 'ubuntu'
-env.hosts = ['35.227.35.75', '100.24.37.33']
 
 
 def do_pack():
-    """
-    Targging project directory into a packages as .tgz
-    """
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
-    local('sudo mkdir -p ./versions')
-    path = './versions/web_static_{}'.format(now)
-    local('sudo tar -czvf {}.tgz web_static'.format(path))
-    name = '{}.tgz'.format(path)
-    if name:
-        return name
+    """The function to configure the archive"""
+    if not (os.path.isdir("versions")):
+        local("mkdir versions")
+    pat = "versions/web_static_{}.tgz".format(
+           datetime.strftime(datetime.now(), "%Y%m%d%H%M%S"))
+    result = local("tar -czvf {} web_static".format(pat))
+    if (result.failed):
+        return (None)
     else:
-        return None
+        return (pat)
 
 
 def do_deploy(archive_path):
-    """Deploy the boxing package tgz file
-    """
-    try:
-        archive = archive_path.split('/')[-1]
-        path = '/data/web_static/releases/' + archive.strip('.tgz')
-        current = '/data/web_static/current'
-        put(archive_path, '/tmp')
-        run('mkdir -p {}'.format(path))
-        run('tar -xzf /tmp/{} -C {}'.format(archive, path))
-        run('rm /tmp/{}'.format(archive))
-        run('mv {}/web_static/* {}'.format(path, path))
-        run('rm -rf {}/web_static'.format(path))
-        run('rm -rf {}'.format(current))
-        run('ln -s {} {}'.format(path, current))
-        print('New version deployed!')
-        return True
-    except:
-        return False
+    """function to deploy the code"""
+    fil = os.path.basename(archive_path)
+    folder = fil.replace(".tgz", "")
+    path = "/data/web_static/releases/{}/".format(folder)
+    yes = False
+    if os.path.exists(archive_path):
+        try:
+            put(archive_path, '/tmp/')
+            run("mkdir -p {}".format(path))
+            run("tar -xzf /tmp/{} -C {}".format(fil, path))
+            run("rm -rf /tmp/{}".format(fil))
+            run("mv {}web_static/* {}".format(path, path))
+            run("rm -rf {}web_static".format(path))
+            run("rm -rf /data/web_static/current")
+            run("ln -sf {} /data/web_static/current".format(path))
+            print('Ran SUccesfully')
+            yes = True
+        except Exception as e:
+            print(e)
+            yes = False
+    else:
+        return (False)
+    return (yes)
 
 
 def deploy():
-    """
-    A function to call do_pack and do_deploy
-    """
-    archive_path = do_pack()
-    answer = do_deploy(archive_path)
-    return answer
+    """overall deployment"""
+    result = do_pack()
+    final = do_deploy(result) if result else False
+    return(final)
